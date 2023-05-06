851cd30ab01ed6eda16a1e99e5cec8156c5676da
Mario <101608787+marioCoding@users.noreply.github.com>
2023-05-06 12:57:00 -0700

    Update bgGenerator.js

--- js/bgGenerator.js
+++ js/bgGenerator.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', init);
 
 async function init() {
-  let color = await fetch("https://www.colr.org/json/color/random");
+  let color = await fetch("https://www.colr.org/json/color/random/");
   let res = await color.json();
   try {
     document.body.style.backgroundColor = `#${await res.new_color}`;
