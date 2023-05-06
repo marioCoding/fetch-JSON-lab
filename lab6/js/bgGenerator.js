a0f8f2b9f4c65998c2d365ad422fde22bfcb7603
Mario <101608787+marioCoding@users.noreply.github.com>
2023-05-06 01:21:56 -0700

    Add files via upload

---
+++ lab6/js/bgGenerator.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', init);
+
+async function init() {
+  let color = await fetch("https://www.colr.org/json/color/random");
+  let res = await color.json();
+  try {
+    document.body.style.backgroundColor = `#${await res.new_color}`;
+  } catch {
+    err => console.error('error', err.message);
+  }
+}
