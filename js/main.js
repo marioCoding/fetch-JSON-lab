69b68a5998e3a48771e78570beec7b9792d80920
Mario <101608787+marioCoding@users.noreply.github.com>
2023-05-06 02:40:59 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,29 +1,24 @@
-fetch("houses.json")
-    .then((response) => response.json())
-    .then((data) => {
-        //create a temp holder to append all the html generated inside the forEach iterator
-        let html = "";
+document.addEventListener('DOMContentLoaded', initialize);
 
-        //the argument "house" passed to the arrow function
-        //holds each item in the array in turn.
-        data.forEach((house) => {
-            let family = house.members.join(" | ");
-
-            // generate the html snippet for one array item
-            //to be added to the "html" temp holder.
-            // format using a description list
-            let objInfo = `<dl class="house">${house.name}</dl>
-        <dd class="folks">${family}</dd>`;
-            html += objInfo;
-        });
-
+async function initialize() {
+    let act = await fetch("http://www.boredapi.com/api/activity/");
+    let activity = await act.json();
+    try {
+        const activityElement = document.createElement("h2");
+        activityElement.textContent = activity.activity;
+        const typeElement = document.createElement("p");
+        typeElement.textContent = activity.type;
+        const participantElement = document.createElement("p");
+        participantElement.textContent = activity.participants;
         //make a reference to the html container where
         //the info will be displayed.
         const container = document.querySelector("#container");
-        container.innerHTML = html;
-        // add css style
-       
-
-    })
-    .catch((err) => console.log("Oops!", err));
+        container.innerHTML = " ";
+        container.appendChild(activityElement);
+        container.appendChild(typeElement);
+        container.appendChild(participantElement);
+    } catch {
+        err => console.log("error", err.message);
+    }
+  };
     //this only runs if there is an error during the above process
