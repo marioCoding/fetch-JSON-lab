1ee1131cdf303e1b333025e055017d88de414391
Mario <101608787+marioCoding@users.noreply.github.com>
2023-05-06 13:02:50 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', initialize);
 
 async function initialize() {
-    let act = await fetch("https://www.boredapi.com/api/activity/")
+    let act = await fetch("https://www.boredapi.com/api/activity")
     let activity = await act.json();
     try {
         const activityElement = document.createElement("h3");
